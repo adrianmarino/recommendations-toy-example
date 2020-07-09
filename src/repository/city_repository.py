1b01025d817b08e60878882ec9e3e0a1a8b2565b
adrianmarino <adrianmarino@gmail.com>
2020-07-09 09:52:04 -0300

    feat: improve step descriptions

--- src/repository/city_repository.py
+++ src/repository/city_repository.py
@@ -2,5 +2,6 @@ from .repository import Repository
 
 class CityRepository(Repository):
     def _upset_query_fn(self, session, entity):
-        session.run("MERGE (c:City {_id: $id}) ON CREATE SET c.iata_code = $iata_code, c.name = $name", 
-                    id=entity.id, iata_code=entity.iata_code, name=entity.name)
+        session.run(
+            "MERGE (c:City {_id: $id}) ON CREATE SET c.iata_code = $iata_code, c.name = $name", 
+            id=entity.id, iata_code=entity.iata_code, name=entity.name)
